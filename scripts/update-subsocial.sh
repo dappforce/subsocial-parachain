2215e67bd2bc8fa2a0e714f6b51c259dc6ef88c2
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-01-15 13:07:58 +0200

    Add Dockerfile and utility scripts

---
+++ scripts/update-subsocial.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+BIN_PATH=$PROJECT_DIR/target/release/subsocial-collator
+
+CUSTOM_SPEC_PATH=$PROJECT_DIR/res/custom-subsocial.json
+RAW_SPEC_PATH=$PROJECT_DIR/res/subsocial.json
+
+$BIN_PATH build-spec --chain subsocial-latest --disable-default-bootnode > "$CUSTOM_SPEC_PATH"
+$BIN_PATH build-spec --chain "$CUSTOM_SPEC_PATH"  --disable-default-bootnode --raw > "$RAW_SPEC_PATH"
+
+rm "$CUSTOM_SPEC_PATH"
