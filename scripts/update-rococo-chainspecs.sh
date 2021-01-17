524cdd032c18b05451ec84f8ab28b8127cd9572e
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-01-17 11:01:14 +0200

    Update Rococo chainspecs

--- scripts/update-rococo-chainspecs.sh
+++ scripts/update-rococo-chainspecs.sh
@@ -7,9 +7,9 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 docker pull parity/rococo:rococo-v1
 docker run --rm -d --name rococo-export parity/rococo:rococo-v1 --chain rococo-local > /dev/null
 
-docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain rococo-local > /rococo-local"
+docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain rococo-local --disable-default-bootnode > /rococo-local"
 docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain rococo > /rococo"
-docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain /rococo-local --raw > /rococo-local.json"
+docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain /rococo-local --raw --disable-default-bootnode > /rococo-local.json"
 docker exec -it -u root rococo-export sh -c "polkadot build-spec --chain /rococo --raw > /rococo.json"
 
 docker cp rococo-export:/rococo-local.json $PROJECT_DIR/res/rococo-local.json
