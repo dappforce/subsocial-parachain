1b582ba78e4b319b957ed43a30b7e6acbffbb649
Alex Siman <aleksandr.siman@gmail.com>
2021-11-13 23:40:17 +0200

    Update runtime/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -141,7 +141,7 @@ impl WeightToFeePolynomial for WeightToFee {
 	type Balance = Balance;
 	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
 		// in Rococo, extrinsic base weight (smallest non-zero weight) is mapped to 1 MILLIUNIT:
-		// in Subsocial node, we map to 1/10 of that, or 1/10 MILLIUNIT
+		// in the Subsocial node, we map to 1/10 of that, or 1/10 MILLIUNIT
 		let p = MILLIUNIT / 10;
 		let q = 100 * Balance::from(ExtrinsicBaseWeight::get());
 		smallvec![WeightToFeeCoefficient {
