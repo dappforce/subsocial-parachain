a875be00900646fd5323c5a93711e1f5b878dde3
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-11-14 00:27:16 +0200

    Fix address prefix

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -259,7 +259,7 @@ parameter_types! {
 		})
 		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
 		.build_or_panic();
-	pub const SS58Prefix: u16 = 42;
+	pub const SS58Prefix: u16 = 28;
 }
 
 // Configure FRAME pallets to include in runtime.
