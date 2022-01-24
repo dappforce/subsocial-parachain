3adb2335afe1bc667ed0277b386d9a56978e05e6
Vladyslav Proshchavaiev <f3joule@gmail.com>
2022-01-24 19:26:32 +0200

    Fix cli.rs and command.rs
    - Add corresponding changes to chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -67,7 +67,7 @@ pub fn subsocial_session_keys(keys: AuraId) -> subsocial_parachain_runtime::Sess
 	subsocial_parachain_runtime::SessionKeys { aura: keys }
 }
 
-pub fn development_config(id: ParaId) -> ChainSpec {
+pub fn development_config() -> ChainSpec {
 	ChainSpec::from_genesis(
 		// Name
 		"Development",
@@ -95,7 +95,7 @@ pub fn development_config(id: ParaId) -> ChainSpec {
 					(get_account_id_from_seed::<sr25519::Public>("Eve"), TESTNET_DEFAULT_ENDOWMENT),
 					(get_account_id_from_seed::<sr25519::Public>("Ferdie"), TESTNET_DEFAULT_ENDOWMENT),
 				],
-				id,
+				DEFAULT_PARA_ID.into(),
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 			)
 		},
@@ -105,12 +105,12 @@ pub fn development_config(id: ParaId) -> ChainSpec {
 		Some(subsocial_properties()),
 		Extensions {
 			relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-			para_id: id.into(),
+			para_id: DEFAULT_PARA_ID,
 		},
 	)
 }
 
-pub fn local_testnet_config(id: ParaId, relay_chain: String) -> ChainSpec {
+pub fn local_testnet_config(relay_chain: String) -> ChainSpec {
 	ChainSpec::from_genesis(
 		// Name
 		"Local Subsocial Testnet",
@@ -138,7 +138,7 @@ pub fn local_testnet_config(id: ParaId, relay_chain: String) -> ChainSpec {
 					(get_account_id_from_seed::<sr25519::Public>("Eve"), TESTNET_DEFAULT_ENDOWMENT),
 					(get_account_id_from_seed::<sr25519::Public>("Ferdie"), TESTNET_DEFAULT_ENDOWMENT),
 				],
-				id,
+				DEFAULT_PARA_ID.into(),
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 			)
 		},
@@ -153,17 +153,17 @@ pub fn local_testnet_config(id: ParaId, relay_chain: String) -> ChainSpec {
 		// Extensions
 		Extensions {
 			relay_chain,
-			para_id: id.into(),
+			para_id: DEFAULT_PARA_ID,
 		},
 	)
 }
 
-pub fn rococo_local_testnet_config(id: ParaId) -> ChainSpec {
-	local_testnet_config(id, "rococo-local".into())
+pub fn rococo_local_testnet_config() -> ChainSpec {
+	local_testnet_config("rococo-local".into())
 }
 
-pub fn kusama_local_testnet_config(id: ParaId) -> ChainSpec {
-	local_testnet_config(id, "kusama-local".into())
+pub fn kusama_local_testnet_config() -> ChainSpec {
+	local_testnet_config("kusama-local".into())
 }
 
 pub fn subsocial_config() -> Result<ChainSpec, String> {
