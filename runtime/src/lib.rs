23ffe5dc95a4841d9240d16a84d60015f4984b92
Alex Siman <aleksandr.siman@gmail.com>
2022-02-16 15:17:27 +0300

    Update runtime/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -617,6 +617,8 @@ impl pallet_vesting::Config for Runtime {
 	type BlockNumberToBalance = ConvertInto;
 	type MinVestedTransfer = MinVestedTransfer;
 	type WeightInfo = ();
+	// `VestingInfo` encode length is 36bytes. 28 schedules gets encoded as 1009 bytes, which is the
+	// highest number of schedules that encodes less than 2^10.
 	const MAX_VESTING_SCHEDULES: u32 = 28;
 }
 
