9a6afd4bc77851a1a2f5e3796b6c0cecf07370d7
Alex Siman <aleksandr.siman@gmail.com>
2022-04-09 15:52:05 +0300

    Revert "WIP: Add initial domains pallet code"
    This reverts commit 9a0f6fa0db6ab1d217327b7e3243593a98e0fff7.

--- pallets/parachain-utils/src/mock_functions.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-pub fn valid_content_ipfs() -> Content {
-    Content::IPFS(b"QmRAQB6YaCaidP37UdDnjFY5aQuiBrbqdyoW1CaDgwxkD4".to_vec())
-}
-
-pub fn invalid_content_ipfs() -> Content {
-    Content::IPFS(b"QmRAQB6DaazhR8".to_vec())
-}
