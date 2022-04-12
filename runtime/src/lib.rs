aa2b46d7abe7c133427824ca89e3b8511407c6cf
Vladyslav Proshchavaiev <f3joule@gmail.com>
2022-04-12 18:56:20 +0300

    Update spec_version to 8

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subsocial-parachain"),
 	impl_name: create_runtime_str!("subsocial-parachain"),
 	authoring_version: 1,
-	spec_version: 7,
+	spec_version: 8,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
