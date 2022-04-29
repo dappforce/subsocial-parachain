6ff5cb048acd296a9aad3c1dc862490ac571aab9
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2022-04-29 02:26:06 +0200

    calls

---
+++ pallets/influencer-staking/src/lib.rs
@@ -0,0 +1,334 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+///! # Influencer staking module.
+///! This module contains the functionality for the influencer staking.
+///!
+///! ## Overview
+///! The influencer staking module is a simple staking system that allows users to stake tokens for
+///! a given user (influencer). The staking system is designed to be used by influencers to incentivize
+///! their social contributes. Rewards from staking inflation is splitterd between the stakers and the
+///! influencer.
+
+
+pub mod types;
+
+use frame_support::traits::Currency;
+pub use pallet::*;
+
+pub(crate) type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::pallet::Config>::AccountId>>::Balance;
+
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_support::metadata::StorageEntryModifier::Default;
+    use frame_support::traits::ReservableCurrency;
+    use frame_system::pallet_prelude::*;
+    use sp_runtime::ArithmeticError;
+    use sp_runtime::traits::{CheckedAdd, CheckedSub, One, Zero};
+    use crate::BalanceOf;
+    use crate::types::{InfluencerInfo, RewardSplitConfig, Round, RoundIndex, StakerInfo};
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        /// The currency trait.
+        type Currency: ReservableCurrency<Self::AccountId>;
+
+        #[pallet::constant]
+        type MaxUnlockingChunks: Get<u32>;
+
+        #[pallet::constant]
+        type InfluenceRegistrationDeposit: Get<BalanceOf<Self>>;
+
+        #[pallet::constant]
+        type MinStake: Get<BalanceOf<Self>>;
+    }
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub (super) trait Store)]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(_);
+
+    #[pallet::storage]
+    pub type RewardConfig<T: Config> = StorageValue<
+        _,
+        RewardSplitConfig,
+        ValueQuery,
+    >;
+
+    #[pallet::storage]
+    pub type CurrentRound<T: Config> = StorageValue<
+        _,
+        Round<T::BlockNumber>,
+        ValueQuery,
+    >;
+
+    #[pallet::storage]
+    pub type Influencers<T: Config> = StorageMap<
+        _,
+        Twox64Concat,
+        T::AccountId,
+        InfluencerInfo<T>,
+        OptionQuery,
+    >;
+
+    #[pallet::storage]
+    pub type Stakers<T: Config> = StorageMap<
+        _,
+        Twox64Concat,
+        T::AccountId,
+        StakerInfo<T>,
+        OptionQuery,
+    >;
+
+    #[pallet::storage]
+    pub type StakedPerInfluencerPerStaker<T: Config> = StorageDoubleMap<
+        _,
+        Twox64Concat,
+        T::AccountId, // influencer
+        Twox64Concat,
+        T::AccountId, // staker
+        BalanceOf<T>,
+        ValueQuery,
+    >;
+
+    /// Total capital locked by this pallet.
+    #[pallet::storage]
+    pub(crate) type Total<T: Config> = StorageValue<
+        _,
+        BalanceOf<T>,
+        ValueQuery,
+    >;
+
+    /// Total capital locked by this pallet.
+    #[pallet::storage]
+    pub(crate) type StakedPerRound<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        RoundIndex,
+        BalanceOf<T>,
+        ValueQuery,
+    >;
+
+    // Pallets use events to inform users when important changes are made.
+    // https://docs.substrate.io/v3/runtime/events-and-errors
+    #[pallet::event]
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        InfluencerRegistered {
+            influencer: T::AccountId,
+        },
+        InfluencerUnregistered {
+            influencer: T::AccountId,
+        },
+    }
+
+    // Errors inform users that something went wrong.
+    #[pallet::error]
+    pub enum Error<T> {
+        StakerDNE,
+        InfluencerDNE,
+        InfluencerAlreadyRegistered,
+        NotStakedForInfluencer,
+        StakeTooLow,
+        RemainingStakeTooLow,
+        NotEnoughStake,
+        InsufficientBalance,
+        UnstakingWithNoValue,
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_initialize(_n: BlockNumberFor<T>) -> Weight {
+            todo!()
+        }
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000)]
+        pub fn register_influencer(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+            let influencer = ensure_signed(origin)?;
+            ensure!(!Self::is_influencer(&influencer), Error::<T>::InfluencerAlreadyRegistered);
+
+            let deposit = T::InfluenceRegistrationDeposit::get();
+            T::Currency::reserve(&influencer, deposit)?;
+
+            <Influencers<T>>::insert(
+                &influencer,
+                InfluencerInfo::from_account(influencer.clone(), deposit),
+            );
+
+            Self::deposit_event(Event::InfluencerRegistered { influencer });
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn unregister_influencer(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+            let influencer = ensure_signed(origin)?;
+
+            let influencer_info = <Influencers<T>>::get(&influencer)
+                .ok_or(Error::<T>::InfluencerDNE)?;
+
+            T::Currency::unreserve(&influencer, influencer_info.deposit);
+
+            <Influencers<T>>::remove(&influencer);
+
+            Self::deposit_event(Event::InfluencerUnregistered { influencer });
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn stake(
+            origin: OriginFor<T>,
+            influencer: T::AccountId,
+            value: BalanceOf<T>,
+        ) -> DispatchResultWithPostInfo {
+            let staker = ensure_signed(origin)?;
+            ensure!(value >= T::MinStake::get(), Error::<T>::StakeTooLow);
+            ensure!(
+				T::Currency::can_reserve(&staker, value),
+				Error::<T>::InsufficientBalance
+			);
+
+            let mut influencer_info = <Influencers<T>>::get(&influencer)
+                .ok_or(Error::<T>::InfluencerDNE)?;
+
+            let mut staker_info = <Stakers<T>>::get(&staker)
+                .unwrap_or_else(|| StakerInfo::<T>::from_account(staker.clone(), Zero::zero()));
+
+            Self::stake_for_influencer(&mut staker_info, &mut influencer_info, value)?;
+
+            <Total<T>>::mutate(|total_stake| *total_stake += value);
+            <Influencers<T>>::insert(&influencer, influencer_info);
+            <Stakers<T>>::insert(&staker, staker_info);
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn unstake(
+            origin: OriginFor<T>,
+            influencer: T::AccountId,
+            value: BalanceOf<T>,
+        ) -> DispatchResultWithPostInfo {
+            let staker = ensure_signed(origin)?;
+            ensure!(value > Zero::zero(), Error::<T>::UnstakingWithNoValue);
+
+            let mut staker_info = <Stakers<T>>::get(&staker)
+                .ok_or(Error::<T>::StakerDNE)?;
+
+            let mut influencer_info = <Influencers<T>>::get(&influencer)
+                .ok_or(Error::<T>::InfluencerDNE)?;
+
+            Self::unstake_form_influencer(&mut staker_info, &mut influencer_info, value)?;
+
+            <Total<T>>::mutate(|total_stake| *total_stake -= value);
+            <Influencers<T>>::insert(&influencer, influencer_info);
+            <Stakers<T>>::insert(&staker, staker_info);
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn unstake_all(
+            origin: OriginFor<T>,
+            influencer: T::AccountId,
+        ) -> DispatchResultWithPostInfo {
+            let staker = ensure_signed(origin)?;
+
+            let mut staker_info = <Stakers<T>>::get(&staker)
+                .ok_or(Error::<T>::StakerDNE)?;
+
+            let mut influencer_info = <Influencers<T>>::get(&influencer)
+                .ok_or(Error::<T>::InfluencerDNE)?;
+
+            let prev_stake = Self::unstake_all_form_influencer(&mut staker_info, &mut influencer_info)?;
+
+            <Total<T>>::mutate(|total_stake| *total_stake -= prev_stake);
+            <Influencers<T>>::insert(&influencer, influencer_info);
+            <Stakers<T>>::insert(&staker, staker_info);
+
+            Ok(().into())
+        }
+    }
+
+    impl<T: Config> Pallet<T> {
+        fn is_influencer(who: &T::AccountId) -> bool {
+            <Influencers<T>>::contains_key(who)
+        }
+
+        fn stake_for_influencer(
+            staker_info: &mut StakerInfo<T>,
+            influencer_info: &mut InfluencerInfo<T>,
+            stake: BalanceOf<T>,
+        ) -> Result<(), DispatchError> {
+            staker_info.total = staker_info.total.checked_add(&stake).ok_or(ArithmeticError::Overflow)?;
+            staker_info.active = staker_info.active.checked_add(&stake).ok_or(ArithmeticError::Overflow)?;
+
+            let influencer = &influencer_info.id;
+
+            let total_stake = match staker_info.staked_per_influencer.get(influencer) {
+                Some(stake) => stake.checked_add(&stake).ok_or(ArithmeticError::Overflow)?,
+                None => {
+                    influencer_info.stakers_count.checked_add(One::one()).ok_or(ArithmeticError::Overflow)?;
+                    stake
+                },
+            };
+
+            influencer_info.staked_amount = influencer_info.staked_amount.checked_add(&stake).ok_or(ArithmeticError::Overflow)?;
+            staker_info.staked_per_influencer.insert(influencer.clone(), total_stake);
+
+            Ok(())
+        }
+
+        fn unstake_form_influencer(
+            staker_info: &mut StakerInfo<T>,
+            influencer_info: &mut InfluencerInfo<T>,
+            stake: BalanceOf<T>,
+        ) -> Result<(), DispatchError> {
+            staker_info.total = staker_info.total.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+            staker_info.active = staker_info.active.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+
+            let influencer = &influencer_info.id;
+
+            let current_stake = staker_info.staked_per_influencer.get(influencer).ok_or(Error::<T>::NotStakedForInfluencer)?;
+            if stake > *current_stake {
+                return Err(Error::<T>::RemainingStakeTooLow.into());
+            }
+
+            let remaining_stake = current_stake.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+            if remaining_stake < T::MinStake::get() {
+                return Err(Error::<T>::RemainingStakeTooLow.into());
+            }
+
+            influencer_info.staked_amount = influencer_info.staked_amount.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+            staker_info.staked_per_influencer.insert(influencer.clone(), remaining_stake);
+
+            Ok(().into())
+        }
+
+        fn unstake_all_form_influencer(
+            staker_info: &mut StakerInfo<T>,
+            influencer_info: &mut InfluencerInfo<T>,
+        ) -> Result<BalanceOf<T>, DispatchError> {
+            let influencer = &influencer_info.id;
+
+            let stake = staker_info.staked_per_influencer.get(influencer).ok_or(Error::<T>::NotStakedForInfluencer)?;
+
+            staker_info.total = staker_info.total.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+            staker_info.active = staker_info.active.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+
+            influencer_info.staked_amount = influencer_info.staked_amount.checked_sub(&stake).ok_or(ArithmeticError::Underflow)?;
+            influencer_info.stakers_count.checked_sub(One::one()).ok_or(ArithmeticError::Underflow)?;
+            staker_info.staked_per_influencer.remove(influencer);
+
+            Ok(stake)
+        }
+    }
+}
---
+++ pallets/influencer-staking/src/types.rs
@@ -0,0 +1,177 @@
+use std::collections::BTreeMap;
+use sp_std::ops::Add;
+use frame_support::pallet_prelude::*;
+use sp_std::prelude::*;
+use sp_runtime::{ArithmeticError, Perbill, traits::CheckedAdd};
+use frame_support::sp_std;
+use sp_runtime::traits::Zero;
+use crate::{BalanceOf, Config};
+
+#[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct RewardSplitConfig {
+    pub influencer_percentage: Perbill,
+    pub stakers_percentage: Perbill,
+}
+
+impl RewardSplitConfig {
+    pub fn new(influencer_percentage: Perbill, stakers_percentage: Perbill) -> Option<Self> {
+        let config = RewardSplitConfig::new_unchecked(influencer_percentage, stakers_percentage);
+        if config.is_valid() {
+            Some(config)
+        } else {
+            None
+        }
+    }
+
+    pub fn new_unchecked(influencer_percentage: Perbill, stakers_percentage: Perbill) -> Self {
+        Self {
+            influencer_percentage,
+            stakers_percentage,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        match self.influencer_percentage.checked_add(&self.stakers_percentage) {
+            None => false,
+            Some(x) if x != Perbill::one() => false,
+            _ => true,
+        }
+    }
+}
+
+impl Default for RewardSplitConfig {
+    fn default() -> Self {
+        Self {
+            influencer_percentage: Perbill::from_percent(50),
+            stakers_percentage: Perbill::from_percent(50),
+        }
+    }
+}
+
+
+/// Type to index staking rounds.
+pub(crate) type RoundIndex = u32;
+
+/// The current round index and transition information
+#[derive(Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub struct Round<BlockNumber> {
+    /// Current round index
+    pub current: RoundIndex,
+    /// The first block of the current round
+    pub first: BlockNumber,
+    /// The length of the current round in number of blocks
+    pub length: u32,
+}
+
+impl<
+    B: Copy + Add<Output = B> + sp_std::ops::Sub<Output = B> + From<u32> + PartialOrd,
+> Default for Round<B>
+{
+    fn default() -> Round<B> {
+        Round::new(1u32, 1u32.into(), 20u32)
+    }
+}
+
+impl<
+    B: Copy + Add<Output = B> + sp_std::ops::Sub<Output = B> + From<u32> + PartialOrd,
+> Round<B>
+{
+    pub fn new(current: RoundIndex, first: B, length: u32) -> Round<B> {
+        Round {
+            current,
+            first,
+            length,
+        }
+    }
+    /// Check if the round should be updated
+    pub fn should_update(&self, now: B) -> bool {
+        now - self.first >= self.length.into()
+    }
+    /// New round
+    pub fn update(&mut self, now: B) {
+        self.current = self.current.saturating_add(1u32);
+        self.first = now;
+    }
+}
+
+
+#[derive(Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo)]
+#[scale_info(skip_type_params(T))]
+pub struct InfluencerInfo<T: Config> {
+    /// Staker account
+    pub id: T::AccountId,
+
+    /// The deposit used for registration.
+    pub deposit: BalanceOf<T>,
+
+    /// The total number of stakers to this influencer.
+    pub stakers_count: u32,
+
+    /// The total amount of tokens staked to this influencer.
+    pub staked_amount: BalanceOf<T>,
+}
+
+impl<T: Config> InfluencerInfo<T> {
+    pub fn from_account(account: T::AccountId, deposit: BalanceOf<T>) -> Self {
+        Self {
+            id: account,
+            deposit,
+            stakers_count: 0,
+            staked_amount: Zero::zero(),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub enum StakerStatus {
+    /// Active with no scheduled exit
+    Active,
+    /// Schedule exit to revoke all ongoing delegations
+    Leaving(RoundIndex),
+}
+
+
+#[derive(Clone, PartialEq, Encode, Decode, RuntimeDebug, TypeInfo)]
+#[scale_info(skip_type_params(T))]
+pub struct StakerInfo<T: Config> {
+    /// Staker account
+    pub id: T::AccountId,
+
+    /// Total balance that is locked. (active + unlocking)
+    pub total: BalanceOf<T>,
+
+    /// The total amount of balance that will be in stake in the next rounds.
+    pub active: BalanceOf<T>,
+
+    /// Amount of balance staked for each influencer.
+    pub staked_per_influencer: BTreeMap<T::AccountId, BalanceOf<T>>,
+
+    /// Any balance that's in the process of being unlocked.
+    pub unlocking: BoundedVec<UnlockChunk<BalanceOf<T>>, T::MaxUnlockingChunks>,
+
+    /// The status of the staker.
+    pub status: StakerStatus,
+}
+
+impl<T: Config> StakerInfo<T> {
+    pub fn from_account(account: T::AccountId, stake: BalanceOf<T>) -> Self {
+        Self {
+            id: account,
+            total: stake,
+            active: stake,
+            unlocking: Default::default(),
+            status: StakerStatus::Active,
+            staked_per_influencer: Default::default(),
+        }
+    }
+}
+
+/// Just a Balance/BlockNumber tuple to encode when a chunk of funds will be unlocked.
+#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub struct UnlockChunk<Balance> {
+    /// Amount of funds to be unlocked.
+    pub value: Balance,
+
+    /// Era number at which point it'll be unlocked.
+    pub round: RoundIndex,
+}
