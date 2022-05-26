e9cc2c9e60455b47a97f5ee525628c24f1ef4039
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2022-05-26 16:43:06 +0200

    Add mocks

--- pallets/energy/src/lib.rs
+++ pallets/energy/src/lib.rs
@@ -2,6 +2,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(test)]
+mod mock;
+
 pub use pallet::*;
 
 
---
+++ pallets/energy/src/mock.rs
@@ -0,0 +1,219 @@
+use frame_support::{
+    assert_ok, dispatch::DispatchResult, parameter_types,
+    traits::{Currency, Everything},
+};
+use frame_support::pallet_prelude::{DispatchClass, Pays, Weight};
+use frame_support::traits::ConstU8;
+use frame_support::weights::{ConstantMultiplier, WeightToFeeCoefficients, WeightToFeeCoefficient, WeightToFeePolynomial, DispatchInfo};
+use frame_support::weights::constants::ExtrinsicBaseWeight;
+use pallet_transaction_payment::{ChargeTransactionPayment, MultiplierUpdate};
+use frame_system::{
+	limits::{BlockLength, BlockWeights},
+	EnsureRoot,
+};
+use smallvec::smallvec;
+use sp_core::H256;
+use sp_io::TestExternalities;
+use sp_runtime::{Perbill, testing::Header, traits::{BlakeTwo256, IdentityLookup}};
+use sp_std::convert::{TryInto, TryFrom};
+
+pub(crate) use crate as pallet_energy;
+
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+pub(super) type AccountId = u64;
+pub(super) type Balance = u64;
+type BlockNumber = u64;
+
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system,
+        Balances: pallet_balances,
+		TransactionPayment: pallet_transaction_payment,
+		Energy: pallet_energy,
+	}
+);
+
+// fn a () {
+// 	let MockBlockWeights: BlockWeights = BlockWeights::builder()
+// 		.base_block(0)
+// 		.for_class(DispatchClass::all(), |weights| {
+// 			weights.max_total = 1_000_000_000_000.into();
+// 		})
+// 		.avg_block_initialization(Perbill::zero())
+// 		.build_or_panic();
+// }
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+	pub MockBlockWeights: BlockWeights = BlockWeights::builder()
+		.base_block(0)
+		.for_class(DispatchClass::all(), |weights| {
+		    weights.base_extrinsic = 0;
+			weights.max_extrinsic = 1_000_000_000.into();
+			weights.max_total = 1_000_000_000_000.into();
+			weights.reserved = None;
+		})
+		.avg_block_initialization(Perbill::zero())
+		.build_or_panic();
+}
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = Everything;
+	type BlockWeights = MockBlockWeights;
+	type BlockLength = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = BlockNumber;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+
+parameter_types! {
+    pub const ExistentialDeposit: u64 = 0;
+}
+
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = ();
+}
+
+
+/// It returns the input weight as the result.
+///
+/// Equals to: f(x) = x
+pub struct IdentityWeightToFeePolynomial;
+impl WeightToFeePolynomial for IdentityWeightToFeePolynomial {
+	type Balance = Balance;
+	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
+		smallvec![WeightToFeeCoefficient {
+			degree: 1,
+			negative: false,
+			coeff_frac: Perbill::zero(),
+			coeff_integer: 1,
+		}]
+	}
+}
+
+/// It returns zero as the result no matter what the input is.
+///
+/// Equals to: f(x) = 0
+pub struct ZeroWeightToFeePolynomial;
+impl WeightToFeePolynomial for ZeroWeightToFeePolynomial {
+	type Balance = Balance;
+	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
+		smallvec![WeightToFeeCoefficient {
+			degree: 0,
+			negative: false,
+			coeff_frac: Perbill::zero(),
+			coeff_integer: 0,
+		}]
+	}
+}
+
+parameter_types! {
+	pub const TransactionByteFee: Balance = 0;
+	pub const OperationalFeeMultiplier: u8 = 0;
+}
+
+impl pallet_transaction_payment::Config for Test {
+	type OnChargeTransaction = Energy;
+	type OperationalFeeMultiplier = ConstU8<0>;
+	type WeightToFee = IdentityWeightToFeePolynomial;
+	type LengthToFee = ZeroWeightToFeePolynomial;
+	type FeeMultiplierUpdate = ();
+}
+
+#[test]
+fn test_that_pallet_transaction_payment_works_as_expected() {
+	assert_eq!(ZeroWeightToFeePolynomial::calc(&4000), 0);
+	assert_eq!(ZeroWeightToFeePolynomial::calc(&1), 0);
+
+	assert_eq!(IdentityWeightToFeePolynomial::calc(&4000), 4000);
+	assert_eq!(IdentityWeightToFeePolynomial::calc(&1), 1);
+
+	fn compute_fee(len: u32, weight: Weight, tip: Balance) -> Balance {
+		ExtBuilder::default().build().execute_with(|| {
+			pallet_transaction_payment::Pallet::<Test>::compute_fee(
+				len,
+				&DispatchInfo {
+					weight,
+					class: DispatchClass::Normal,
+					pays_fee: Pays::Yes,
+				},
+				tip,
+			)
+		})
+	}
+
+	assert_eq!(compute_fee(0, 0, 0), 0);
+	assert_eq!(compute_fee(0, 1, 0), 1);
+	assert_eq!(compute_fee(0, 1, 1), 2);
+	assert_eq!(compute_fee(1, 1, 1), 2);
+	assert_eq!(compute_fee(10_000, 1, 1), 2);
+	assert_eq!(compute_fee(10_000, 10_000, 1), 10_001);
+	assert_eq!(compute_fee(100_000, 10_000, 10_000), 20_000);
+}
+
+impl pallet_energy::Config for Test {
+	type Event = Event;
+	type Currency = Balances;
+	type FallbackOnChargeTransaction = pallet_transaction_payment::CurrencyAdapter<Balances, ()>;
+}
+
+
+pub struct ExtBuilder {
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		ExtBuilder {
+		}
+	}
+}
+
+impl ExtBuilder {
+	pub(crate) fn build(self) -> TestExternalities {
+		let storage = &mut frame_system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.unwrap();
+
+		let mut ext = TestExternalities::from(storage.clone());
+		ext.execute_with(|| {
+			System::set_block_number(1);
+		});
+
+		ext
+	}
+}
