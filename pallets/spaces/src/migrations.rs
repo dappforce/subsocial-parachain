1f06f05948ec0d65c3051a29543b417b4b551507
Vladyslav Proshchavaiev <f3joule@gmail.com>
2022-05-31 13:17:10 +0300

    WIP: Upgrade `pallet-spaces` to the new proc macro
    Co-Authored-By: Shady Khalifa <shekohex@gmail.com>

--- pallets/spaces/src/migrations.rs
+++ pallets/spaces/src/migrations.rs
@@ -1,4 +1,3 @@
-use frame_support::storage::IterableStorageMap;
 use sp_runtime::traits::Saturating;
 
 use pallet_utils as Utils;
@@ -10,11 +9,11 @@ pub fn fix_corrupted_handles_storage<T: Config>() -> frame_support::weights::Wei
     let mut should_remove;
     let mut removed = 0;
 
-    for (handle, space_id) in SpaceIdByHandle::iter() {
+    for (handle, space_id) in SpaceIdByHandle::<T>::iter() {
         handles_iterated += 1;
         should_remove = false;
 
-        if let Some(space) = Module::<T>::space_by_id(&space_id) {
+        if let Some(space) = Pallet::<T>::space_by_id(&space_id) {
             let space_handle_lc = space.handle.map(Utils::Module::<T>::lowercase_handle);
 
             if space_handle_lc.is_none() || space_handle_lc.as_ref() != Some(&handle) {
@@ -25,15 +24,12 @@ pub fn fix_corrupted_handles_storage<T: Config>() -> frame_support::weights::Wei
         }
 
         if should_remove {
-            SpaceIdByHandle::remove(handle);
+            SpaceIdByHandle::<T>::remove(handle);
             removed += 1;
         }
     }
 
-    SpaceIdByHandleStorageFixed::put(true);
+    SpaceIdByHandleStorageFixed::<T>::put(true);
 
-    T::DbWeight::get().reads_writes(
-        handles_iterated.saturating_mul(2),
-        removed + 1,
-    )
+    T::DbWeight::get().reads_writes(handles_iterated.saturating_mul(2), removed + 1)
 }
