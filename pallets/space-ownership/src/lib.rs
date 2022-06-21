28fc0b2f11ca97e4e8532aeacb1b77ebf8cc2741
Vladyslav Proshchavaiev <f3joule@gmail.com>
2022-06-21 12:04:42 +0300

    WIP Upgrade `pallet-space-ownership` to the new proc macro
    Co-Authored-By: Shady Khalifa <shekohex@gmail.com>

--- pallets/space-ownership/src/lib.rs
+++ pallets/space-ownership/src/lib.rs
@@ -1,129 +1,170 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage,
-    ensure,
-    dispatch::DispatchResult,
-    traits::Get
+    decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult, ensure,
+    traits::Get,
 };
-use sp_std::prelude::*;
 use frame_system::{self as system, ensure_signed};
+use sp_std::prelude::*;
 
 use df_traits::moderation::IsAccountBlocked;
 use pallet_spaces::{Module as Spaces, SpaceById, SpaceIdsByOwner};
-use pallet_utils::{Error as UtilsError, SpaceId, remove_from_vec};
+use pallet_utils::{remove_from_vec, Error as UtilsError, SpaceId};
 
-/// The pallet's configuration trait.
-pub trait Config: system::Config
-    + pallet_utils::Config
-    + pallet_spaces::Config
-{
-    /// The overarching event type.
-    type Event: From<Event<Self>> + Into<<Self as system::Config>::Event>;
-}
+pub use pallet::*;
 
-decl_error! {
-  pub enum Error for Module<T: Config> {
-    /// The current space owner cannot transfer ownership to themself.
-    CannotTranferToCurrentOwner,
-    /// Account is already an owner of a space.
-    AlreadyASpaceOwner,
-    /// There is no pending ownership transfer for a given space.
-    NoPendingTransferOnSpace,
-    /// Account is not allowed to accept ownership transfer.
-    NotAllowedToAcceptOwnershipTransfer,
-    /// Account is not allowed to reject ownership transfer.
-    NotAllowedToRejectOwnershipTransfer,
-  }
-}
+#[frame_support::pallet]
+pub mod pallet {
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
 
-// This pallet's storage items.
-decl_storage! {
-    trait Store for Module<T: Config> as SpaceOwnershipModule {
-        pub PendingSpaceOwner get(fn pending_space_owner):
-            map hasher(twox_64_concat) SpaceId => Option<T::AccountId>;
-    }
-}
-
-decl_event!(
-    pub enum Event<T> where
-        <T as system::Config>::AccountId,
-    {
-        SpaceOwnershipTransferCreated(/* current owner */ AccountId, SpaceId, /* new owner */ AccountId),
-        SpaceOwnershipTransferAccepted(AccountId, SpaceId),
-        SpaceOwnershipTransferRejected(AccountId, SpaceId),
-    }
-);
-
-// The pallet's dispatchable functions.
-decl_module! {
-  pub struct Module<T: Config> for enum Call where origin: T::Origin {
-
-    // Initializing errors
-    type Error = Error<T>;
-
-    // Initializing events
-    fn deposit_event() = default;
-
-    #[weight = 10_000 + T::DbWeight::get().reads_writes(1, 1)]
-    pub fn transfer_space_ownership(origin, space_id: SpaceId, transfer_to: T::AccountId) -> DispatchResult {
-      let who = ensure_signed(origin)?;
-
-      let space = Spaces::<T>::require_space(space_id)?;
-      space.ensure_space_owner(who.clone())?;
-
-      ensure!(who != transfer_to, Error::<T>::CannotTranferToCurrentOwner);
-      ensure!(T::IsAccountBlocked::is_allowed_account(transfer_to.clone(), space_id), UtilsError::<T>::AccountIsBlocked);
-
-      <PendingSpaceOwner<T>>::insert(space_id, transfer_to.clone());
-
-      Self::deposit_event(RawEvent::SpaceOwnershipTransferCreated(who, space_id, transfer_to));
-      Ok(())
+    #[pallet::config]
+    pub trait Config: frame_system::Config + pallet_utils::Config + pallet_spaces::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
 
-    #[weight = 10_000 + T::DbWeight::get().reads_writes(2, 2)]
-    pub fn accept_pending_ownership(origin, space_id: SpaceId) -> DispatchResult {
-      let new_owner = ensure_signed(origin)?;
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(PhantomData<T>);
 
-      let mut space = Spaces::require_space(space_id)?;
-      ensure!(!space.is_owner(&new_owner), Error::<T>::AlreadyASpaceOwner);
-
-      let transfer_to = Self::pending_space_owner(space_id).ok_or(Error::<T>::NoPendingTransferOnSpace)?;
-      ensure!(new_owner == transfer_to, Error::<T>::NotAllowedToAcceptOwnershipTransfer);
-
-      // Here we know that the origin is eligible to become a new owner of this space.
-      <PendingSpaceOwner<T>>::remove(space_id);
-
-      Spaces::maybe_transfer_handle_deposit_to_new_space_owner(&space, &new_owner)?;
-
-      let old_owner = space.owner;
-      space.owner = new_owner.clone();
-      <SpaceById<T>>::insert(space_id, space);
-
-      // Remove space id from the list of spaces by old owner
-      <SpaceIdsByOwner<T>>::mutate(old_owner, |space_ids| remove_from_vec(space_ids, space_id));
-
-      // Add space id to the list of spaces by new owner
-      <SpaceIdsByOwner<T>>::mutate(new_owner.clone(), |ids| ids.push(space_id));
-
-      // TODO add a new owner as a space follower? See T::BeforeSpaceCreated::before_space_created(new_owner.clone(), space)?;
-
-      Self::deposit_event(RawEvent::SpaceOwnershipTransferAccepted(new_owner, space_id));
-      Ok(())
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_runtime_upgrade() -> Weight {
+            let old_pallet_prefix = "SpaceOwnershipModule";
+            let new_pallet_prefix = Self::name();
+            frame_support::log::info!(
+                "Move Storage from {} to {}",
+                old_pallet_prefix,
+                new_pallet_prefix
+            );
+            frame_support::migration::move_pallet(
+                old_pallet_prefix.as_bytes(),
+                new_pallet_prefix.as_bytes(),
+            );
+            T::BlockWeights::get().max_block
+        }
     }
 
-    #[weight = 10_000 + T::DbWeight::get().reads_writes(2, 1)]
-    pub fn reject_pending_ownership(origin, space_id: SpaceId) -> DispatchResult {
-      let who = ensure_signed(origin)?;
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+        pub fn transfer_space_ownership(
+            origin: OriginFor<T>,
+            space_id: SpaceId,
+            transfer_to: T::AccountId,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
 
-      let space = Spaces::<T>::require_space(space_id)?;
-      let transfer_to = Self::pending_space_owner(space_id).ok_or(Error::<T>::NoPendingTransferOnSpace)?;
-      ensure!(who == transfer_to || who == space.owner, Error::<T>::NotAllowedToRejectOwnershipTransfer);
+            let space = Spaces::<T>::require_space(space_id)?;
+            space.ensure_space_owner(who.clone())?;
 
-      <PendingSpaceOwner<T>>::remove(space_id);
+            ensure!(who != transfer_to, Error::<T>::CannotTranferToCurrentOwner);
+            ensure!(
+                T::IsAccountBlocked::is_allowed_account(transfer_to.clone(), space_id),
+                UtilsError::<T>::AccountIsBlocked
+            );
 
-      Self::deposit_event(RawEvent::SpaceOwnershipTransferRejected(who, space_id));
-      Ok(())
+            PendingSpaceOwner::<T>::insert(space_id, transfer_to.clone());
+
+            Self::deposit_event(Event::SpaceOwnershipTransferCreated(
+                who,
+                space_id,
+                transfer_to,
+            ));
+            Ok(())
+        }
+
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2, 2))]
+        pub fn accept_pending_ownership(origin: OriginFor<T>, space_id: SpaceId) -> DispatchResult {
+            use frame_support::StorageMap;
+
+            let new_owner = ensure_signed(origin)?;
+
+            let mut space = Spaces::require_space(space_id)?;
+            ensure!(!space.is_owner(&new_owner), Error::<T>::AlreadyASpaceOwner);
+
+            let transfer_to =
+                Self::pending_space_owner(space_id).ok_or(Error::<T>::NoPendingTransferOnSpace)?;
+            ensure!(
+                new_owner == transfer_to,
+                Error::<T>::NotAllowedToAcceptOwnershipTransfer
+            );
+
+            // Here we know that the origin is eligible to become a new owner of this space.
+            PendingSpaceOwner::<T>::remove(space_id);
+
+            Spaces::maybe_transfer_handle_deposit_to_new_space_owner(&space, &new_owner)?;
+
+            let old_owner = space.owner;
+            space.owner = new_owner.clone();
+            SpaceById::<T>::insert(space_id, space);
+
+            // Remove space id from the list of spaces by old owner
+            SpaceIdsByOwner::<T>::mutate(old_owner, |space_ids| {
+                remove_from_vec(space_ids, space_id)
+            });
+
+            // Add space id to the list of spaces by new owner
+            SpaceIdsByOwner::<T>::mutate(new_owner.clone(), |ids| ids.push(space_id));
+
+            // TODO add a new owner as a space follower? See T::BeforeSpaceCreated::before_space_created(new_owner.clone(), space)?;
+
+            Self::deposit_event(Event::SpaceOwnershipTransferAccepted(new_owner, space_id));
+            Ok(())
+        }
+
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2, 1))]
+        pub fn reject_pending_ownership(origin: OriginFor<T>, space_id: SpaceId) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let space = Spaces::<T>::require_space(space_id)?;
+            let transfer_to =
+                Self::pending_space_owner(space_id).ok_or(Error::<T>::NoPendingTransferOnSpace)?;
+            ensure!(
+                who == transfer_to || who == space.owner,
+                Error::<T>::NotAllowedToRejectOwnershipTransfer
+            );
+
+            PendingSpaceOwner::<T>::remove(space_id);
+
+            Self::deposit_event(Event::SpaceOwnershipTransferRejected(who, space_id));
+            Ok(())
+        }
     }
-  }
+
+    #[pallet::event]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        SpaceOwnershipTransferCreated(
+            /* current owner */ T::AccountId,
+            SpaceId,
+            /* new owner */ T::AccountId,
+        ),
+        SpaceOwnershipTransferAccepted(T::AccountId, SpaceId),
+        SpaceOwnershipTransferRejected(T::AccountId, SpaceId),
+    }
+
+    /// Old name generated by `decl_event`.
+    #[deprecated(note = "use `Event` instead")]
+    pub type RawEvent<T> = Event<T>;
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The current space owner cannot transfer ownership to themself.
+        CannotTranferToCurrentOwner,
+        /// Account is already an owner of a space.
+        AlreadyASpaceOwner,
+        /// There is no pending ownership transfer for a given space.
+        NoPendingTransferOnSpace,
+        /// Account is not allowed to accept ownership transfer.
+        NotAllowedToAcceptOwnershipTransfer,
+        /// Account is not allowed to reject ownership transfer.
+        NotAllowedToRejectOwnershipTransfer,
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn pending_space_owner)]
+    pub type PendingSpaceOwner<T: Config> = StorageMap<_, Twox64Concat, SpaceId, T::AccountId>;
 }
