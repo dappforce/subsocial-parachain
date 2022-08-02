4f0d06bfc862b1b59f6e285f21946ef39c15519f
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2022-08-02 14:15:56 +0200

    add more benchmarks

--- pallets/roles/src/benchmarking.rs
+++ pallets/roles/src/benchmarking.rs
@@ -6,7 +6,7 @@ use super::*;
 use sp_std::vec;
 use frame_system::RawOrigin;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
-use subsocial_support::Content;
+use subsocial_support::{Content, User};
 use frame_support::{
     dispatch::DispatchError,
     traits::Currency,
@@ -23,7 +23,7 @@ fn create_dummy_space<T: Config + pallet_spaces::Config>(origin: RawOrigin<T::Ac
     let space_id = pallet_spaces::NextSpaceId::<T>::get();
 
     pallet_spaces::Pallet::<T>::create_space(
-        origin.clone().into(),
+        origin.into(),
         None,
         Content::None,
         None,
@@ -36,23 +36,83 @@ fn create_dummy_space<T: Config + pallet_spaces::Config>(origin: RawOrigin<T::Ac
 }
 
 
+fn create_dummy_role<T: Config>(
+    origin: RawOrigin<T::AccountId>,
+    space_id: SpaceId,
+) -> Result<Role<T>, DispatchError> {
+    let role_id = NextRoleId::<T>::get();
+
+    Pallet::<T>::create_role(
+        origin.into(),
+        space_id,
+        Some(100u32.into()),
+        Content::None,
+        vec![SP::ManageRoles],
+    )?;
+
+    let role = RoleById::<T>::get(role_id)
+        .ok_or(DispatchError::Other("Role not found"))?;
+
+    Ok(role)
+}
+
 benchmarks! {
     where_clause { where T: pallet_spaces::Config }
 
     create_role {
-        let caller = account::<T::AccountId>("Acc1", 1, 0);
-        let space = create_dummy_space::<T>(RawOrigin::Signed(caller.clone().into()))?;
+        let caller_origin = RawOrigin::Signed(account::<T::AccountId>("Acc1", 1, 0));
+        let space = create_dummy_space::<T>(caller_origin.clone())?;
         let time_to_live: Option<T::BlockNumber> = Some(100u32.into());
         let content = Content::IPFS(b"QmRAQB6YaCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4".to_vec());
         let perms = vec![SP::ManageRoles];
         let role_id = NextRoleId::<T>::get();
-    }: _(RawOrigin::Signed(caller.clone()), space.id, time_to_live, content, perms)
+    }: _(caller_origin, space.id, time_to_live, content, perms)
     verify {
         let role = RoleById::<T>::get(role_id).unwrap();
         let space_roles_ids = RoleIdsBySpaceId::<T>::get(space.id);
 
         ensure!(role.id == role_id, "Role id doesn't match");
         ensure!(space_roles_ids.contains(&role_id), "Role id not in space roles");
+    }
+
+    update_role {
+        let caller_origin = RawOrigin::Signed(account::<T::AccountId>("Acc1", 1, 0));
+        let space = create_dummy_space::<T>(caller_origin.clone())?;
+        let role = create_dummy_role::<T>(caller_origin.clone(), space.id)?;
+
+        ensure!(!role.disabled, "Role should be enabled");
+
+        let update = RoleUpdate {
+             disabled: true.into(),
+             content: Content::IPFS(b"QmRAQB6YaCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4".to_vec()).into(),
+             permissions: None
+        };
+    }: _(caller_origin, role.id, update)
+    verify {
+        let role = RoleById::<T>::get(role.id).unwrap();
+        ensure!(role.disabled, "Role should be disabled");
+    }
+
+    delete_role {
+        let caller_origin = RawOrigin::Signed(account::<T::AccountId>("Acc1", 1, 0));
+        let space = create_dummy_space::<T>(caller_origin.clone())?;
+        let role = create_dummy_role::<T>(caller_origin.clone(), space.id)?;
+    }: _(caller_origin, role.id)
+    verify {
+        let deleted = RoleById::<T>::get(role.id).is_none();
+        ensure!(deleted, "Role should be deleted");
+    }
+
+    grant_role {
+        let caller_origin = RawOrigin::Signed(account::<T::AccountId>("Acc1", 1, 0));
+        let space = create_dummy_space::<T>(caller_origin.clone())?;
+        let role = create_dummy_role::<T>(caller_origin.clone(), space.id)?;
+
+        let users_to_grant = vec![User::Account(account::<T::AccountId>("Acc2", 2, 0))];
+    }: _(caller_origin, role.id, users_to_grant.clone())
+    verify {
+        let granted_users = UsersByRoleId::<T>::get(role.id);
+        ensure!(granted_users == users_to_grant, "Role should be deleted");
     }
 
     impl_benchmark_test_suite!(
