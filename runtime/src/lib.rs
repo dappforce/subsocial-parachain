7e6f0e97d521f91cdd9094a290af3972249dfc78
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2022-08-09 15:55:34 +0300

    Update spec_version to 12 (#133)
    - Update transaction_version to 2

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -164,10 +164,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subsocial-parachain"),
 	impl_name: create_runtime_str!("subsocial-parachain"),
 	authoring_version: 1,
-	spec_version: 12,
+	spec_version: 13,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 	state_version: 0,
 };
 
