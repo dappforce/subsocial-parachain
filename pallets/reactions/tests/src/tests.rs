144fac9c25373774c4d00c56465da6f6b6fbb03e
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2023-02-16 22:35:31 +0200

    Add tests for update/delete reaction

--- pallets/reactions/tests/src/tests.rs
+++ pallets/reactions/tests/src/tests.rs
@@ -3,7 +3,7 @@ use frame_system::pallet_prelude::OriginFor;
 
 use pallet_posts::{Error as PostsError, Post, PostById, PostExtension, PostUpdate};
 use pallet_reactions::{Error as ReactionsError, ReactionKind};
-use pallet_reactions::Event::PostReactionCreated;
+use pallet_reactions::Event::{PostReactionCreated, PostReactionDeleted, PostReactionUpdated};
 use pallet_spaces::types::{Space, SpaceUpdate};
 use subsocial_support::{Content, SpaceId};
 
@@ -123,7 +123,7 @@ fn create_post_reaction_should_work_downvote() {
         assert_eq!(Reactions::reaction_ids_by_post_id(post.id), vec![reaction_id]);
         assert_eq!(Reactions::next_reaction_id(), next_reaction_id);
 
-        // Check post reaction counters again
+        // Check post reaction counters
         let post = Posts::post_by_id(post.id).unwrap();
         assert_eq!(post.upvotes_count, 0);
         assert_eq!(post.downvotes_count, 1);
@@ -192,5 +192,162 @@ fn create_post_reaction_should_fail_when_trying_to_react_on_hidden_post() {
             Reactions::create_post_reaction(origin, post.id, ReactionKind::Upvote),
             ReactionsError::<Test>::CannotReactWhenPostHidden
         );
+    });
+}
+
+
+#[test]
+fn update_post_reaction_should_fail_when_reaction_not_found() {
+    ExtBuilder::build().execute_with(|| {
+        let origin = Origin::signed(13);
+        let post = create_post(origin.clone());
+        let non_existing_reaction_id = 56;
+
+        assert_noop!(
+            Reactions::update_post_reaction(origin, post.id, non_existing_reaction_id, ReactionKind::Upvote),
+            ReactionsError::<Test>::ReactionByAccountNotFound
+        );
+    });
+}
+
+#[test]
+fn update_post_reaction_should_fail_when_not_reaction_owner() {
+    ExtBuilder::build().execute_with(|| {
+        let owner_origin = Origin::signed(13);
+        let not_owner_origin = Origin::signed(123);
+
+        let post = create_post(owner_origin.clone());
+
+        let reaction_id = Reactions::next_reaction_id();
+        assert_ok!(Reactions::create_post_reaction(owner_origin, post.id, ReactionKind::Upvote));
+
+
+        assert_noop!(
+            Reactions::update_post_reaction(not_owner_origin, post.id, reaction_id, ReactionKind::Downvote),
+            ReactionsError::<Test>::ReactionByAccountNotFound
+        );
+    });
+}
+
+#[test]
+fn update_post_reaction_should_fail_when_setting_same_reaction() {
+    ExtBuilder::build().execute_with(|| {
+        let origin = Origin::signed(432);
+
+        let post = create_post(origin.clone());
+        let reaction_id = Reactions::next_reaction_id();
+        assert_ok!(Reactions::create_post_reaction(origin.clone(), post.id, ReactionKind::Upvote));
+
+
+        assert_noop!(
+            Reactions::update_post_reaction(origin, post.id, reaction_id, ReactionKind::Upvote),
+            ReactionsError::<Test>::SameReaction
+        );
+    });
+}
+
+#[test]
+fn update_post_reaction_should_work() {
+    ExtBuilder::build().execute_with(|| {
+        let account = 423;
+        let origin = Origin::signed(account);
+
+        let post = create_post(origin.clone());
+        let reaction_id = Reactions::next_reaction_id();
+        assert_ok!(Reactions::create_post_reaction(origin.clone(), post.id, ReactionKind::Upvote));
+
+        let post = Posts::post_by_id(post.id).unwrap();
+        assert_eq!(post.upvotes_count, 1);
+        assert_eq!(post.downvotes_count, 0);
+
+        assert_ok!(
+            Reactions::update_post_reaction(origin, post.id, reaction_id, ReactionKind::Downvote),
+        );
+
+        System::assert_last_event(PostReactionUpdated {
+            account,
+            post_id: post.id,
+            reaction_id,
+            reaction_kind: ReactionKind::Downvote,
+        }.into());
+
+        let post = Posts::post_by_id(post.id).unwrap();
+        assert_eq!(post.upvotes_count, 0);
+        assert_eq!(post.downvotes_count, 1);
+
+        // Check whether data stored correctly
+        let reaction = Reactions::reaction_by_id(reaction_id).unwrap();
+        assert_eq!(reaction.created.account, account);
+        assert_eq!(reaction.kind, ReactionKind::Downvote);
+    });
+}
+
+//////
+
+
+#[test]
+fn delete_post_reaction_should_fail_when_reaction_not_found() {
+    ExtBuilder::build().execute_with(|| {
+        let origin = Origin::signed(13);
+        let post = create_post(origin.clone());
+        let non_existing_reaction_id = 56;
+
+        assert_noop!(
+            Reactions::delete_post_reaction(origin, post.id, non_existing_reaction_id),
+            ReactionsError::<Test>::ReactionByAccountNotFound
+        );
+    });
+}
+
+#[test]
+fn delete_post_reaction_should_fail_when_not_reaction_owner() {
+    ExtBuilder::build().execute_with(|| {
+        let owner_origin = Origin::signed(13);
+        let not_owner_origin = Origin::signed(123);
+
+        let post = create_post(owner_origin.clone());
+
+        let reaction_id = Reactions::next_reaction_id();
+        assert_ok!(Reactions::create_post_reaction(owner_origin, post.id, ReactionKind::Upvote));
+
+
+        assert_noop!(
+            Reactions::delete_post_reaction(not_owner_origin, post.id, reaction_id),
+            ReactionsError::<Test>::ReactionByAccountNotFound
+        );
+    });
+}
+
+#[test]
+fn delete_post_reaction_should_work() {
+    ExtBuilder::build().execute_with(|| {
+        let account = 423;
+        let origin = Origin::signed(account);
+
+        let post = create_post(origin.clone());
+        let reaction_id = Reactions::next_reaction_id();
+        assert_ok!(Reactions::create_post_reaction(origin.clone(), post.id, ReactionKind::Upvote));
+
+        let post = Posts::post_by_id(post.id).unwrap();
+        assert_eq!(post.upvotes_count, 1);
+        assert_eq!(post.downvotes_count, 0);
+
+        assert_ok!(
+            Reactions::delete_post_reaction(origin, post.id, reaction_id),
+        );
+
+        System::assert_last_event(PostReactionDeleted {
+            account,
+            post_id: post.id,
+            reaction_id,
+            reaction_kind: ReactionKind::Upvote,
+        }.into());
+
+        let post = Posts::post_by_id(post.id).unwrap();
+        assert_eq!(post.upvotes_count, 0);
+        assert_eq!(post.downvotes_count, 0);
+
+        // Check whether data is deleted
+        assert!(Reactions::reaction_by_id(reaction_id).is_none());
     });
 }
