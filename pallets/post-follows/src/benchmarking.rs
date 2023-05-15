a51e19ba2dfc1f918d47ed9f1693ee23be42857f
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-05-15 13:19:21 +0300

    Fix benchmarks

--- pallets/post-follows/src/benchmarking.rs
+++ pallets/post-follows/src/benchmarking.rs
@@ -2,22 +2,43 @@
 
 #![cfg(feature = "runtime-benchmarks")]
 
-use super::*;
 use frame_benchmarking::{account, benchmarks};
 use frame_support::{dispatch::DispatchError, ensure};
 use frame_system::RawOrigin;
-use pallet_posts::types::Post;
-use subsocial_support::Content;
+
+use pallet_posts::{types::Post, PostExtension, PostById, NextPostId};
+use pallet_spaces::types::Space;
+use subsocial_support::{Content, SpaceId};
+
+use super::*;
+
+fn create_dummy_space<T: Config>(
+    origin: RawOrigin<T::AccountId>,
+) -> Result<Space<T>, DispatchError> {
+    let space_id = pallet_spaces::NextSpaceId::<T>::get();
+
+    pallet_spaces::Pallet::<T>::create_space(origin.into(), Content::None, None)?;
+
+    let space = pallet_spaces::SpaceById::<T>::get(space_id)
+        .ok_or(DispatchError::Other("Space not found"))?;
+
+    Ok(space)
+}
 
 fn create_dummy_post<T: Config>(
     origin: RawOrigin<T::AccountId>,
+    space_id: SpaceId,
 ) -> Result<Post<T>, DispatchError> {
-    let post_id = pallet_posts::NextPostId::<T>::get();
+    let post_id = NextPostId::<T>::get();
 
-    pallet_posts::Pallet::<T>::create_post(origin.clone().into(), Content::None, None)?;
+    pallet_posts::Pallet::<T>::create_post(
+        origin.into(),
+        Some(space_id),
+        PostExtension::RegularPost,
+        Content::None,
+    )?;
 
-    let post = pallet_posts::PostById::<T>::get(post_id)
-        .ok_or(DispatchError::Other("Post not found"))?;
+    let post = PostById::<T>::get(post_id).ok_or(DispatchError::Other("Post wasn't created"))?;
 
     Ok(post)
 }
@@ -25,10 +46,11 @@ fn create_dummy_post<T: Config>(
 benchmarks! {
 
     follow_post {
-        let post_owner_origin = RawOrigin::Signed(account::<T::AccountId>("PostOwner", 2, 0));
+        let owner_origin = RawOrigin::Signed(account::<T::AccountId>("PostOwner", 2, 0));
         let post_follower = account::<T::AccountId>("PostFollower", 1, 0);
 
-        let post = create_dummy_post::<T>(post_owner_origin.clone())?;
+        let space = create_dummy_space::<T>(owner_origin.clone())?;
+        let post = create_dummy_post::<T>(owner_origin.clone(), space.id)?;
     }: _(RawOrigin::Signed(post_follower.clone()), post.id)
     verify {
         ensure!(PostFollowers::<T>::get(post.id).contains(&post_follower), "PostFollowers was not updated");
@@ -37,10 +59,11 @@ benchmarks! {
     }
 
     unfollow_post {
-        let post_owner_origin = RawOrigin::Signed(account::<T::AccountId>("PostOwner", 2, 0));
+        let owner_origin = RawOrigin::Signed(account::<T::AccountId>("PostOwner", 2, 0));
         let post_follower = account::<T::AccountId>("PostFollower", 1, 0);
 
-        let post = create_dummy_post::<T>(post_owner_origin.clone())?;
+        let space = create_dummy_space::<T>(owner_origin.clone())?;
+        let post = create_dummy_post::<T>(owner_origin.clone(), space.id)?;
         Pallet::<T>::follow_post(RawOrigin::Signed(post_follower.clone()).into(),post.id)?;
 
     }: _(RawOrigin::Signed(post_follower.clone()), post.id)
