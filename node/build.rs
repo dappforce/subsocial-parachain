0a37e8add181e7108cc72206f2f0b79d3a89ad18
Tushar Ojha <tusharojha2001@gmail.com>
2023-06-26 03:17:34 +0530

    added copyright texts to files

--- node/build.rs
+++ node/build.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0.
+// Copyright (C) 2021-2022 DAPPFORCE PTE. Ltd., aleksandr.siman@gmail.com.
+
+// Full notice is available at https://github.com/dappforce/subsocial-parachain/blob/main/HEADER-GPL3. 
+// Full license is available at https://github.com/dappforce/subsocial-parachain/blob/main/LICENSE.
+
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
--- pallets/support/src/traits.rs
+++ pallets/support/src/traits.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0.
+// Copyright (C) 2021-2022 DAPPFORCE PTE. Ltd., aleksandr.siman@gmail.com.
+
+// Full notice is available at https://github.com/dappforce/subsocial-parachain/blob/main/HEADER-GPL3. 
+// Full license is available at https://github.com/dappforce/subsocial-parachain/blob/main/LICENSE.
+
 pub use common::{
     ProfileManager, SpaceFollowsProvider, SpacePermissionsProvider, SpacesInterface, PostFollowsProvider,
 };
