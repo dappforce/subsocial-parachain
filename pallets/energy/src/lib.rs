2a25b522cc49024abe4833bab64d18f0fc828570
Tushar Ojha <tusharojha2001@gmail.com>
2023-06-28 16:59:11 +0530

    added review changes

--- pallets/energy/src/lib.rs
+++ pallets/energy/src/lib.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0.
-// Copyright (C) 2021-2022 DAPPFORCE PTE. Ltd., aleksandr.siman@gmail.com.
+// Copyright (C) 2021-2023 DAPPFORCE PTE. Ltd., dappforce@gmail.com.
 
 // Full notice is available at https://github.com/dappforce/subsocial-parachain/blob/main/HEADER-GPL3. 
 // Full license is available at https://github.com/dappforce/subsocial-parachain/blob/main/LICENSE.
