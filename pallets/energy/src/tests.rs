cd8891d96a0ff6b8f7afdbdf01cc11f52522e764
Tushar Ojha <tusharojha2001@gmail.com>
2023-08-01 23:54:00 +0530

    updated copyright text in all files

--- pallets/energy/src/tests.rs
+++ pallets/energy/src/tests.rs
@@ -1,5 +1,5 @@
+// Copyright (C) DAPPFORCE PTE. LTD., dappforce@gmail.com.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0.
-// Copyright (C) 2021-2023 DAPPFORCE PTE. Ltd., dappforce@gmail.com.
 
 // Full notice is available at https://github.com/dappforce/subsocial-parachain/blob/main/HEADER-GPL3. 
 // Full license is available at https://github.com/dappforce/subsocial-parachain/blob/main/LICENSE.
