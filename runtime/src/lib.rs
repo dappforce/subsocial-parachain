c01de31de839b02f1d266605b0f34d2802331c6c
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-08-15 09:23:20 +0300

    Update Soonsocial spec_version to 2701
    - Pallet creator staking added

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("soonsocial-parachain"),
 	impl_name: create_runtime_str!("soonsocial-parachain"),
 	authoring_version: 1,
-	spec_version: 2700,
+	spec_version: 2701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
