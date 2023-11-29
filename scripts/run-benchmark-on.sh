bba154e0632384b119d9261014bb9e41deb6e6c7
Alex Siman <aleksandr.siman@gmail.com>
2023-11-29 04:41:46 +0200

    Fix a typo in run-benchmark-on.sh

--- scripts/run-benchmark-on.sh
+++ scripts/run-benchmark-on.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$SCRIPT_DIR/..
 
 if [[ -z $1 || -z $2 ]]; then
-  echo "You have to specify the pallet name and the output dir"
+  echo "You have to specify the pallet name and the output file"
   echo "For example: ./run-benchmark-on.sh pallet_name ./pallets/name/src/weights.rs"
   exit 1
 fi
