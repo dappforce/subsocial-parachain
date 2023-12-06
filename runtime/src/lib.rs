bdf4d2b3b4720b1444ff6f7fbb555b5b0e7899fb
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-12-06 21:39:13 +0200

    Add move stake feature to creator staking pallet (#242)
    * Add move stake feature to creator staking pallet
    
    * Fix call indexes in creator staking pallet
    
    * Rename `calculate_final_unstaking_amount` to `calculate_and_apply_stake_decrease`
    
    * Add tests for move_stake
    
    * Update spec_version to 34
    
    * Update transaction_version to 7
    
    * Improve namings in creator staking testing utils
    
    ---------
    
    Co-authored-by: Alex Siman <siman@users.noreply.github.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -176,10 +176,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subsocial-parachain"),
 	impl_name: create_runtime_str!("subsocial-parachain"),
 	authoring_version: 1,
-	spec_version: 33,
+	spec_version: 34,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 6,
+	transaction_version: 7,
 	state_version: 0,
 };
 
