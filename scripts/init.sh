051af66844ce3d059192a5fa263c2385d4758517
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-12-06 22:55:33 +0200

    Revert toolchain changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,14 +4,18 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-if [ "$1" == "nosudo" ]; then
-   apt-get update && \
-   apt-get install -y build-essential clang curl libssl-dev protobuf-compiler
-else
-   sudo apt-get update && \
-   sudo apt-get install -y build-essential clang curl libssl-dev protobuf-compiler
+CDIR=`dirname "$0"`
+export RUSTC_VERSION=`cat $CDIR/../RUSTC_VERSION`
+
+if [ -z $CI_PROJECT_NAME ] ; then
+   rustup update $RUSTC_VERSION
+   rustup update stable
 fi
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y && \
-   . "$HOME/.cargo/env" && \
-   rustup show
+rustup target add wasm32-unknown-unknown --toolchain $RUSTC_VERSION
+
+# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
+command -v wasm-gc || \
+	cargo +$RUSTC_VERSION install --git https://github.com/alexcrichton/wasm-gc --force
+
+rustup override set $RUSTC_VERSION
