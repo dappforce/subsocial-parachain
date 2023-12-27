d8817373645c5158b62bd718b20c7feecbc276fe
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-12-28 00:47:58 +0200

    Add pallet multisig to the runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -607,6 +607,23 @@ impl pallet_utility::Config for Runtime {
 }
 
 parameter_types! {
+// One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes.
+	pub const DepositBase: Balance = deposit(1, 88);
+	// Additional storage item size of 32 bytes.
+	pub const DepositFactor: Balance = deposit(0, 32);
+}
+
+impl pallet_multisig::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeCall = RuntimeCall;
+	type Currency = Balances;
+	type DepositBase = DepositBase;
+	type DepositFactor = DepositFactor;
+	type MaxSignatories = ConstU32<100>;
+	type WeightInfo = pallet_multisig::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
     pub const MinDomainLength: u32 = 4;
     pub const MaxDomainLength: u32 = 63;
 
@@ -801,6 +818,7 @@ construct_runtime!(
 		Vesting: pallet_vesting = 26,
 		Proxy: pallet_proxy = 27,
 		Utility: pallet_utility = 28,
+		Multisig: pallet_multisig = 29,
 
 		// XCM helpers.
 		XcmpQueue: cumulus_pallet_xcmp_queue = 30,
