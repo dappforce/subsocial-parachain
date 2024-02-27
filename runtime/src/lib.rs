a01fb066f85b8d5ce882de1c4304961bb2740af1
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2024-02-27 14:04:31 +0200

    Update transaction_version to 9

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -181,7 +181,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 41,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 8,
+	transaction_version: 9,
 	state_version: 0,
 };
 
