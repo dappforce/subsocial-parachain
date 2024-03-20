18df14eead08e6d0315d4431e458352daf1e2690
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2024-03-20 17:41:02 +0200

    Update spec_version to 42

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -189,7 +189,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subsocial-parachain"),
 	impl_name: create_runtime_str!("subsocial-parachain"),
 	authoring_version: 1,
-	spec_version: 41,
+	spec_version: 42,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 9,
