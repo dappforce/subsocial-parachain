8045ad8ec03e9c1257a0443d9c21b29b49d3379e
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2024-03-25 13:17:31 +0200

    Soon: fix transaction_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -187,7 +187,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 4101,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 12,
+	transaction_version: 13,
 	state_version: 0,
 };
 
