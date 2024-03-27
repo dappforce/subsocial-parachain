6a6413eb4c7c3d18f3e43f41012af1b7cd565c0f
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2024-03-27 16:57:38 +0200

    Rename space-ownership tests

--- integration-tests/src/utils/ownership_utils.rs
+++ integration-tests/src/utils/ownership_utils.rs
@@ -21,7 +21,7 @@ pub(crate) fn _transfer_space_ownership(
     space_id: Option<SpaceId>,
     transfer_to: Option<AccountId>,
 ) -> DispatchResult {
-    SpaceOwnership::transfer_ownership(
+    Ownership::transfer_ownership(
         origin.unwrap_or_else(|| RuntimeOrigin::signed(ACCOUNT1)),
         space_id.map_or(OwnableEntity::Space(SPACE1), |id| OwnableEntity::Space(id)),
         transfer_to.unwrap_or(ACCOUNT2),
@@ -33,7 +33,7 @@ pub(crate) fn _accept_default_pending_ownership() -> DispatchResult {
 }
 
 pub(crate) fn _accept_pending_ownership(origin: Option<RuntimeOrigin>, space_id: Option<SpaceId>) -> DispatchResult {
-    SpaceOwnership::accept_pending_ownership(
+    Ownership::accept_pending_ownership(
         origin.unwrap_or_else(|| RuntimeOrigin::signed(ACCOUNT2)),
         space_id.map_or(OwnableEntity::Space(SPACE1), |id| OwnableEntity::Space(id)),
     )
@@ -48,7 +48,7 @@ pub(crate) fn _reject_default_pending_ownership_by_current_owner() -> DispatchRe
 }
 
 pub(crate) fn _reject_pending_ownership(origin: Option<RuntimeOrigin>, space_id: Option<SpaceId>) -> DispatchResult {
-    SpaceOwnership::reject_pending_ownership(
+    Ownership::reject_pending_ownership(
         origin.unwrap_or_else(|| RuntimeOrigin::signed(ACCOUNT2)),
         space_id.map_or(OwnableEntity::Space(SPACE1), |id| OwnableEntity::Space(id)),
     )
