000cc297019960838c1e6291190425f113fae45f
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2024-12-24 12:36:25 +0200

    Add missing migrations

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -121,6 +121,11 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
+	(
+		pallet_collator_selection::migration::v1::MigrateToV1<Runtime>,
+		pallet_multisig::migrations::v1::MigrateToV1<Runtime>,
+		pallet_balances::migration::MigrateToTrackInactive<Runtime, xcm_config::CheckAccount>,
+	),
 >;
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
